b05b3c7e3e9f2b5e04aca5451bdbd4d6bd5effa9
MayurSarkar <76404762+MayurSarkar@users.noreply.github.com>
2024-03-18 00:06:35 +0530

    First Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+const Contact = () => {
+    return (
+        <footer id="contact" className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Reach Out to Me:</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                <a href="mailto:myemail@email.com"><img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" /></a>
+                    <a id={styles.contacttext} href="mailto:myemail@email.com">pabitrasarkar1964@gmail.com</a>
+                </li>
+                <li className={styles.link}>
+                <a href="https://www.linkedin.com/in/mayur-sarkar-286523221/"><img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" /></a>
+                    <a id={styles.contacttext} href="https://www.linkedin.com/in/mayur-sarkar-286523221/">linkedin.com/in/mayur-sarkar-286523221</a>
+                </li>
+                <li className={styles.link}>
+                <a href="https://github.com/MayurSarkar"><img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" /></a>
+                    <a id={styles.contacttext} href="https://github.com/MayurSarkar">github.com/MayurSarkar</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Contact
